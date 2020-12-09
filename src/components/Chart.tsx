9d57896da708b416a33fa1b7c3e162e3a4e3e344
matt15y <matt15y@gmail.com>
2020-12-09 10:27:56 -0600

    Fixed error [GraphQL] cannot read property'split' of undefined caused by error destructuing in the useQuery error handler.

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from 'react';
-// import { Line } from 'react-chartjs-2'
-import { useQuery } from 'urql';
+import { makeStyles } from '@material-ui/core/styles';
+import { useQuery, useSubscription } from 'urql';
+import { Line, LineChart, XAxis, YAxis, Tooltip, Label } from 'recharts';
+import ChartIndicator from '../components/ChartIndicator';
 
-import { Line, LineChart, XAxis, YAxis, Tooltip } from 'recharts';
-
-const newQuery = `{
-  getLastKnownMeasurement(metricName:"casingPressure"){value,at}    
-  }`;
+const useStyles = makeStyles({
+  root: {
+    width: 1280,
+    height: 720,
+    backgroundColor: '##dcdde1',
+    paddingTop: 25,
+    paddingBottom: 25,
+    marginTop: 15,
+    marginLeft: 15,
+    display: 'flex',
+  },
+  div: {
+    width: 200,
+    height: 300,
+    marginTop: 15,
+    marginLeft: 15,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 let newData: any = {};
 
@@ -18,15 +39,25 @@ function addTime() {
   console.log(test, 'this from add time');
 }
 
-function Chart() {
+function Chart(props) {
+  const classes = useStyles();
+  console.log(props.value, 'THIS IS VALUE FROM CHARTS');
+  const metricName = props.value;
   const [psiData, setPsiData] = useState([newData]);
+
+  const newQuery = `query($metricName: String!){
+    getLastKnownMeasurement(metricName:$metricName){metric,at,value,unit}    
+    }`;
 
   const [result, reexecuteQuery] = useQuery({
     query: newQuery,
+    variables: { metricName },
     pollInterval: 5000,
     requestPolicy: 'cache-and-network',
   });
   const { data, fetching, error } = result;
+
+  console.log(result, 'THIS IS CONSOLE RESULT');
 
   useEffect(() => {
     count++;
@@ -39,35 +70,43 @@ function Chart() {
     setPsiData(psiData => [...psiData, newData]);
   }, [data]);
 
-  // console.log(psiData, "THIS IS PSIDATA")
-
-  // console.log("YOOO", newData)
+  useEffect(() => {
+    if (metricName === 'test') {
+      console.log(metricName, 'THIS IS INITIAL VALUE');
+    } else {
+      setPsiData(psiData => [(psiData = [])]);
+    }
+  }, [metricName]);
 
   const time = new Date(1234567890);
   const newTime = time.getTime();
   console.log(psiData);
   if (fetching) return <p>Loading...</p>;
-  if (error) return <p>Oh no... {error.message}</p>;
+  if (error) {
+    console.log(error.message);
+    return <p></p>;
+  }
   if (data) {
     newData = data.getLastKnownMeasurement;
     newData.test1 = '233';
     addTime();
   }
-  const testData = psiData[psiData.length - 1]['value'];
 
-  console.log(testData, 'this is psiData');
+  const testData = psiData[psiData.length - 1]['value'];
 
   return (
     <div>
-      <p>{testData}this is PSI Data</p>
-      <h1>Real Time CPU Usage</h1>
-      <LineChart width={1280} height={480} data={psiData}>
-        <XAxis dataKey="at" />
-        <YAxis />
-        <Line dataKey="value" />
-        <Line dataKey="test1" />
-        <Tooltip />
-      </LineChart>
+      <ChartIndicator props={[metricName, testData]} />
+      <div className={classes.root}>
+        <LineChart height={640} width={1280} margin={{ top: 20, right: 30, left: 0, bottom: 0 }} data={psiData}>
+          <XAxis dataKey="at">
+            <Label value="time" offset={0} position="insideLeft" />
+          </XAxis>
+          <YAxis label="test" />
+          <Line dataKey="value" />
+          <Tooltip />
+        </LineChart>
+      </div>
     </div>
   );
 }
