ed0fa07207dd0c632955eaa26a3b6f4231fff8b8
matt15y <matt15y@gmail.com>
2020-12-10 14:49:03 -0600

    Cleanup: removed unused imports

--- src/components/ChartIndicator.tsx
+++ src/components/ChartIndicator.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -30,7 +28,6 @@ const useStyles = makeStyles({
 });
 
 export default function ChartIndicator({ props }) {
-  console.log(props, 'this is props@@@@@@');
   const classes = useStyles();
   return (
     <div className={classes.div}>
