152484874de2f8fb778606bc5466c1af2b702d7b
matt15y <matt15y@gmail.com>
2020-12-10 01:51:10 -0600

    Updated client to reflect subscription and webscocket connection

--- src/components/ChartContainer.tsx
+++ src/components/ChartContainer.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Provider, createClient } from 'urql';
+import { Provider, createClient, subscriptionExchange, defaultExchanges } from 'urql';
 import Chart from './Chart';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+
+const subscriptionClient = new SubscriptionClient('ws://react.eogresources.com/graphql', {});
 
 const client = createClient({
-  url: 'https://react.eogresources.com/graphql',
+  url: 'ws://react.eogresources.com/graphql',
+  exchanges: [
+    ...defaultExchanges,
+    subscriptionExchange({ forwardSubscription: operation => subscriptionClient.request(operation) }),
+  ],
 });
 
 const ChartContainer = ({ value }) => (
