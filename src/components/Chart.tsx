3f12f62ab92c1219e0c679a662ac105b44f3166b
matt15y <matt15y@gmail.com>
2020-12-10 19:14:23 -0600

    Added useEffect to get a better ChartIndicatorValue

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -68,32 +68,32 @@ function Chart(props) {
   const subscriptionData = testThisData();
 
   // State Hook
-  const [psiData, setPsiData] = useState([newData]);
+  const [metricData, setMetricData] = useState([newData]);
 
   // Subscription useEffect
   useEffect(() => {
     // Keeps iterrable array at length 30 so that data is readable
-    if (testData.length >= 30) {
-      testData.shift();
+    if (filteredMetricData.length >= 30) {
+      filteredMetricData.shift();
     }
 
-    if (psiData.length >= 300) {
-      psiData.shift();
+    if (metricData.length >= 300) {
+      metricData.shift();
     }
     // Set state when data (subscription) changes
-    setPsiData(psiData => [...psiData, subscriptionData]);
+    setMetricData(metricData => [...metricData, subscriptionData]);
   }, [data]);
 
   // Conditional useEffect to set empty array when no selection has been made
   useEffect(() => {
     if (metricName === 'initial') {
     } else {
-      setPsiData(psiData => [(psiData = [])]);
+      setMetricData(metricData => [(metricData = [])]);
     }
   }, [metricName]);
 
   // State parsed into an array and time converted to UTC
-  const testData = psiData
+  const filteredMetricData = metricData
     .filter(items => items.metric === metricName)
     .map(items => {
       const dt = new Date(parseInt(items.at) * 1000);
@@ -104,11 +104,34 @@ function Chart(props) {
       return items;
     });
 
+  const reducer = (accumulator, currentValue) => currentValue;
+
+  const filteredValue = filteredMetricData.map(item => item.value);
+
+  const [metricValueProp, setMetricValueProp] = useState(newData);
+
+  // Subscription useEffect
+  useEffect(() => {
+    // Keeps iterrable array at length 30 so that data is readable
+    if (filteredValue.length >= 30) {
+      filteredMetricData.shift();
+    }
+    const updateValue = filteredValue[filteredValue.length - 1];
+    // Set state when data (subscription) changes
+    setMetricValueProp(metricValueProp => [metricValueProp, updateValue]);
+  }, [data]);
+
+  console.log(metricValueProp, '___________ metricValueProp_______________');
   return (
     <div>
-      <ChartIndicator props={[metricName]} />
+      <ChartIndicator props={[metricName, metricValueProp]} />
       <div className={classes.root}>
-        <LineChart height={640} width={1280} margin={{ top: 20, right: 30, left: 20, bottom: 30 }} data={testData}>
+        <LineChart
+          height={640}
+          width={1280}
+          margin={{ top: 20, right: 30, left: 20, bottom: 30 }}
+          data={filteredMetricData}
+        >
           <XAxis dataKey="at" label={{ value: 'Time', position: 'bottom' }} />
           <YAxis />
           <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2} />
